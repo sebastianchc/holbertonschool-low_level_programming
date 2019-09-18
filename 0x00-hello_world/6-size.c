36a93809a3e81ea151733852797b2786f9a50d28
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-09-18 16:56:20 +0000

    Low Level Programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main() {char C; int I; long int L; long long int LL; float F; printf("Size of char: %ld byte(s)\n",sizeof(C)); printf("Size of int: %ld byte(s)\n",sizeof(I)); printf("Size of long int: %ld byte(s)\n",sizeof(L)); printf("Size of long long int: %ld byte(s)\n",sizeof(LL)); printf("Size of float: %ld byte(s)\n",sizeof(F)); return 0;}
