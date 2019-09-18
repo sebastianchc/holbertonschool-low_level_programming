562a7287fa3be87fe27440625931137130440d57
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-09-18 20:23:30 +0000

    Low Level Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char *A = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *A = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, A, 59);
 	return (1);
