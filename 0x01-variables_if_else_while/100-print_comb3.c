858d540cf7dcf411c0efad4a71730c3906792200
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-09-19 22:47:26 +0000

    Variables: If, Else and While

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,28 @@ int main(void)
 {
 	int num1;
 	int num2;
+	int num3;
 
-	num1 = 48;
-	num2 = 49;
-	while (num1 <= 57)
+	num1 = 0;
+	num2 = 1;
+	num3 = 1;
+	while (num1 <= 9)
 	{
-		putchar(num1);
-		num1++;
-		while (num2 <= 57)
+		while (num2 <= 9)
 		{
-			putchar(num2);
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (!(num1 == 8 && num2 == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			num2++;
 		}
+		num1++;
+		num3++;
+		num2 = num3;
 	}
+	putchar('\n');
 	return (0);
 }
