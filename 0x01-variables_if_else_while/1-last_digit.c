609e4060954d790a600d1499c433efbd449809d9
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-09-19 17:20:02 +0000

    Variables: If, Else and While

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ int main(void)
 	printf("%d", last);
 	if (last > 5)
 	{
-		printf(" and is greater than 5 \n");
+		printf(" and is greater than 5\n");
 	}
 	if (last == 0)
 	{
-		printf(" and is 0 \n");
+		printf(" and is 0\n");
 	}
 	else if (last < 6)
 	{
-		printf(" and is less than 6 and not 0 \n");
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }
