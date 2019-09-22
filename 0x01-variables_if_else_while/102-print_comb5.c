d4e6f63f68bb1b6fdcd4c8d33232d06aa991d0c3
sebastianchc <1200@holbertonschool.com>
2019-09-22 02:06:27 +0000

    Variables: If, Else and While

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	x = 0;
+	y = 1;
+	z = 1;
+	while (x <= 98)
+	{
+		while (y <= 99)
+		{
+			if (y > x)
+			{
+				putchar((x / 10) + '0');
+				putchar((x % 10) + '0');
+				putchar(' ');
+				putchar((y / 10) + '0');
+				putchar((y % 10) + '0');
+				if (x < 98 || y < 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		x++;
+		z++;
+		y = z;
+	}
+	putchar('\n');
+	return (0);
+}
