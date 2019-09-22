a1535dd9a78cbb304883c4ce58a5ac0e1fbf8a6a
sebastianchc <1200@holbertonschool.com>
2019-09-22 01:45:33 +0000

    Variables: If, Else and While

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,40 +9,23 @@
 
 int main(void)
 {
-	int num1, num2, num3, num4, num5, num6;
+	int x;
 
-	num1 = 0;
-	num2 = 1;
-	num3 = 1;
-	num4 = 2;
-	num5 = 2;
-	num6 = 2;
-	while (num1 < 9)
+	x = 1;
+	while (x <= 789)
 	{
-		while (num2 < 9)
+		if ((x % 10) > ((x / 10) % 10) && ((x / 10) % 10) > (x / 100))
 		{
-			while (num4 <= 9)
+			putchar((x / 100) + '0');
+			putchar(((x / 10) % 10) + '0');
+			putchar((x % 10) + '0');
+			if (x < 789)
 			{
-				putchar(num1 + '0');
-				putchar(num2 + '0');
-				putchar(num4 + '0');
-				if (!(num1 == 7 && num2 == 8 && num4 == 9))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				num4++;
+				putchar(',');
+				putchar(' ');
 			}
-			num2++;
-			num5++;
-			num4 = num5;
 		}
-		num1++;
-		num3++;
-		num6++;
-		num5 = num6;
-		num2 = num3;
-		num4 = num6;
+		x++;
 	}
 	putchar('\n');
 	return (0);
