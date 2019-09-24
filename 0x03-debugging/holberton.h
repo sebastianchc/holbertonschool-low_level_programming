0f6b18cd0bf5238268e19150b033cfb2d587b8af
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-09-24 21:35:03 +0000

    Debbuging

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -11,3 +11,13 @@ void positive_or_negative(int i);
 #define _LAST_H
 int largest_number(int a, int b, int c);
 #endif
+
+#ifndef _DATE_H
+#define _DATE_H
+void print_remaining_days(int month, int day, int year);
+#endif
+
+#ifndef _DATE1_H
+#define _DATE1_H
+int convert_day(int month, int day);
+#endif
