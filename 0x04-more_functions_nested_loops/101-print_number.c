13f4eaa706771dda9d3f9a1ef9a17e4bf3de2e18
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-09-26 21:41:49 +0000

    More Functions and More Nested Loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int a, z, y, d, x, b, c;
+
+	a = 1052;
+	z = 1052;
+	for (a = z; a > 0; )
+	{
+		for (y = 0; d > 10; y * 10)
+		{
+			for (a = 1052; a != 0; )
+			{
+				a = a / x;
+				b = a % x;
+			}
+			putchar(b + '0');
+			d = z / y;
+			c = z % y;
+		}
+		c = z;
+	}
+	return (0);
+}
