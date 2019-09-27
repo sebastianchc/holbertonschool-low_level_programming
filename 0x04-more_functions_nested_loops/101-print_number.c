5e952f54e18214c072dc9f519c59aa824463d459
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-09-27 19:22:27 +0000

    More Functions and More Nested Loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_number - Print number.
+ * @n: variable.
+ *
+ * Return: Nothing.
+ */
+
+void print_number(int n)
+{
+	int w;
+
+	w = 10;
+	if (n == 0)
+	{
+		_putchar(n + '0');
+	}
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar('-');
+	}
+	while (n > 0)
+	{
+		while (n / w > 0)
+		{
+			w = w * 10;
+		}
+		w = w / 10;
+		_putchar((n / w) + '0');
+		n = n % w;
+	}
+}i
