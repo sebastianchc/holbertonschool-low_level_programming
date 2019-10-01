851cd64f50cccd5ee694b515efb9d59d011ed60e
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-01 14:45:27 +0000

    Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,23 @@
 
 void rev_string(char *s)
 {
-	int x;
+	int  x, y, z;
+	char w;
 
 	x = 0;
 	while (s[x] != '\0')
 	{
 		x++;
+	}
+	x = x - 1;
+	y = 0;
+	z = x;
+	while (y <= z / 2)
+	{
+		w = s[x];
+		s[x] = s[y];
+		s[y] = w;
+		x--;
+		y++;
 	}
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,2 +1,30 @@
 #include "holberton.h"
 
+/**
+ * puts_half - Print half-string.
+ * @str: Variable.
+ *
+ * Return: Nothing.
+ */
+
+void puts_half(char *str)
+{
+	int x, y;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	y = x - 1;
+	x = 0;
+	while (str[x] != '\0')
+	{
+		if (x > y / 2)
+		{
+			_putchar(str[x]);
+		}
+		x++;
+	}
+	_putchar('\n');
+}
