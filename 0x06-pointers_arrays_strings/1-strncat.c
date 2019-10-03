5c9bca7b516a9fd677803d93854b4baffb687250
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-03 18:24:21 +0000

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,5 +11,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int x, y;
+
+	x = 0;
+	y = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (y < n)
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+        return (dest);
 }
