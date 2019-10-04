1ce27ac1fdf54d99e2c600549bd96a17214ddee2
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-04 14:50:03 +0000

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,11 +2,23 @@
 
 /**
  * string_toupper - Changes all lowercase letters of a string to uppercase.
- * @n: Variable.
+ * @src: Variable.
  *
- * Return: Always 0.
+ * Return: *src.
  */
 
-char *string_toupper(char *n)
+char *string_toupper(char *src)
 {
+	int x;
+
+	x = 0;
+	while (src[x] != '\0')
+	{
+		if (src[x] >= 97 && src[x] <= 122)
+		{
+			src[x] = src[x] - 32;
+		}
+		x++;
+	}
+	return (src);
 }
