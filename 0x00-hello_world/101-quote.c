1eebafd323fee282239f4d376285424694f75ed8
sebastianchc <1200@holbertonschool.com>
2019-10-07 00:51:58 +0000

    First Time Programming C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Standar Error.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	char *A = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, A, 59);
+	write(2, x, 59);
 	return (1);
 }
-.
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Printf.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-.
