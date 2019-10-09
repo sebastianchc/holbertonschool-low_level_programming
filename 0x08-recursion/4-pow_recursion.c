05c771da6d0a74ba3be8abe462c4c47068c3f231
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-09 18:11:04 +0000

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -19,8 +19,8 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	else if (y > 0)
-        {
-                return (x * _pow_recursion(x, y - 1));
-        }
-        return (x);
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (x);
 }
