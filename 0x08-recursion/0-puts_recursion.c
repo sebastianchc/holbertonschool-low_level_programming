3491555044766fde2803e5173f4347e23a40dce6
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-09 17:30:52 +0000

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != 0)
+	if (*s != '\0')
 	{
-		_puts_recursion(s);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	if (*s == 0)
+	else
 	{
 		_putchar('\n');
 	}
