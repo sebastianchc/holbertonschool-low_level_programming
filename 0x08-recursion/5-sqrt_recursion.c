af200a50cb8b509c0243774738b6aae09814fce2
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-10 17:31:10 +0000

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _square_root - Count.
+ *
+ * Return: square root.
+ */
+
+int _square_root(int x, int n)
+{
+       	if (x * x <= n)
+	{
+		if (x * x == n)
+		{
+			return (x);
+		}
+		else
+		{
+			return (_square_root(x + 1, n));
+		}
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number.
+ * @n: Variable.
+ *
+ * Return: square root of n.
+ */
+
+int _sqrt_recursion(int n)
+{
+	int x;
+
+	x = 0;
+	return (_square_root(x, n));
+}
