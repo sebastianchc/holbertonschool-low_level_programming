f28550ff4c3be8d1e30e2842cf28ead6385ce5e9
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-15 17:02:33 +0000

    Malloc-Free Project

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,24 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _CREARY_H
+#define _CREARY_H
+char *create_array(unsigned int size, char c);
+#endif
+
+#ifndef _STRDUP_H
+#define _STRDUP_H
+char *_strdup(char *str);
+#endif
+
+#ifndef _STRCON_H
+#define _STRCON_H
+char *str_concat(char *s1, char *s2);
+#endif
+
+#ifndef _ALLGRD_H
+#define _ALLGRD_H
+int **alloc_grid(int width, int height);
+#endif
