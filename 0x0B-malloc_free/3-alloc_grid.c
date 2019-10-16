318e92448eb0a800311288b26d3706fbb0f9ab4f
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-16 18:02:45 +0000

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,30 +3,40 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - Function.
- * @width: Variable.
- * @height: Variable
+ *alloc_grid - g
+ *@width: nn
+ *@height: g
  *
- * Return: x.
+ *Return: array.
  */
 
 int **alloc_grid(int width, int height)
 {
-	int *array;
-	int x, y;
+	int **array;
+	int i, j;
 
-	x = 0;
-	array = (int *)malloc(width * height * sizeof(int));
-	while (x < height)
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0;  i < height; i++)
 	{
-		y = 0;
-		while (y < width)
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
 		{
-			*(array + x * height + y) = 48;
-			y++;
+			for (j = 0; j < height; j++)
+				free(array[j]);
+			free(array);
+			return (NULL);
 		}
-		printf("\n");
-		x++;
 	}
-	return (0);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
 }
