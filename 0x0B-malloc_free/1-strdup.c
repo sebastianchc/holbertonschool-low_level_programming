2cc7fdd97a1be31e5726a129c3af97d36bc6b3d1
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2019-10-16 16:54:20 +0000

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,20 +14,16 @@ char *_strdup(char *str)
 	char *array;
 	int x, y;
 
-	x = 0;
 	if (str == NULL)
 	{
 		return (0);
 	}
+	x = 0;
 	while (str[x])
 	{
 		x++;
 	}
 	array = malloc((x + 1) *  sizeof(char));
-	if (array == 0)
-	{
-		return (0);
-	}
 	y = 0;
 	while (y < x)
 	{
