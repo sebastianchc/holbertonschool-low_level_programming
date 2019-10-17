ec48e25f3aee661d7b039375b91ef972a4ccdefa
sebastianchc <1200@holbertonschool.com>
2019-10-17 18:47:47 +0000

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *array;
 
-	if (new_size == 0 && ptr)
+	if (!ptr)
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	else if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (0);
