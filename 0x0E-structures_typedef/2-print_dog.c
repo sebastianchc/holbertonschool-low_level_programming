700936ef9b2c0a9fe911f4192c5090c26055cf63
sebastianchc <1200@holbertonschool.com>
2019-10-21 17:46:36 +0000

    HP

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 	else
 	{
 		((*d).name ? printf("Name: %s\n", (*d).name) : printf("Name: (nil)\n"));
-		((*d).age ? printf("Age: %1f\n", (*d).age) : printf("age: (nil)\n"));
+		printf("Age: %1f\n", (*d).age);
 		((*d).owner ? printf("Owner: %s\n", (*d).owner) : printf("owner: (nil)\n"));
 	}
 }
