c7d82d7070326d63da61ee8f96d2100422d96117
sebastianchc <1200@holbertonschool.com>
2019-10-21 20:43:31 +0000

    PILL ME

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,18 +60,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (0);
 	}
 	(*ndog).name = _strdup(name);
-       	if (!((*ndog).name))
-       	{
-       		free(ndog);
-	       	return (0);
+	if (!((*ndog).name))
+	{
+		free(ndog);
+		return (0);
 	}
-       	(*ndog).age = age;
-       	(*ndog).owner = _strdup(owner);
-       	if (!((*ndog).owner))
-       	{
-       		free(ndog);
-	       	free((*ndog).name);
-	       	return (0);
-       	}
+	(*ndog).age = age;
+	(*ndog).owner = _strdup(owner);
+	if (!((*ndog).owner))
+	{
+		free(ndog);
+		free((*ndog).name);
+		return (0);
+	}
 	return (ndog);
 }
