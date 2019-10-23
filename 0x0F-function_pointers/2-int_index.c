0844f1da04e92ca33a98ad6c9602688e895596d5
sebastianchc <1200@holbertonschool.com>
2019-10-23 18:28:32 +0000

    Int Index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,11 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp)
-	{
-		return (0);
-	}
-	if (size <= 0)
+	if (!array || !cmp || size <= 0)
 	{
 		return (-1);
 	}
