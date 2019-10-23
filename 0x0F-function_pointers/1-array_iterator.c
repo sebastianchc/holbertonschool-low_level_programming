e2a222f602512879512b7bf74c346f3900ef7c2e
sebastianchc <1200@holbertonschool.com>
2019-10-23 16:59:34 +0000

    UHHHHHHH

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer.
+ * @array: Array.
+ * @size: Size.
+ * @action: Function pointer.
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned i;
+
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,19 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _PRINT_NAME_H
+#define _PRINT_NAME_H
+void print_name(char *name, void (*f)(char *));
+#endif
+
+#ifndef _ARRAY_ITE_H
+#define _ARRAY_ITE_H
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
+
+#ifndef _ARRAY_ITE_H
+#define _ARRAY_ITE_H
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
