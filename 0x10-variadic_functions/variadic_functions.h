3bbb32fb34492d346535e968a26fc370d8afdad2
sebastianchc <1200@holbertonschool.com>
2019-10-26 19:24:05 +0000

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -22,3 +22,20 @@ void print_strings(const char *separator, const unsigned int n, ...);
 #define PRTALL
 void print_all(const char * const format, ...);
 #endif
+
+#ifndef TYPE
+#define TYPE
+#include <stdarg.h>
+
+/**
+ * struct type - Struct.
+ * @t1: Type.
+ * @t2: Function pointer.
+ */
+
+typedef struct type
+{
+	char *t1;
+	void (*t2)(va_list);
+} format_t;
+#endif
