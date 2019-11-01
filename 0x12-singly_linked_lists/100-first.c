33c54fe7fdf610724545ff1c28ff229b59958ea5
sebastianchc <1200@holbertonschool.com>
2019-11-01 02:47:41 +0000

    Before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - Print before main is executed.
+ *
+ * Return: Nothing.
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
