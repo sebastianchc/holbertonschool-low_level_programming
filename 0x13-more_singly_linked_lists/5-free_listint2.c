d5e705f10bb050f0b3911f10148e98fe0c50718e
sebastianchc <1200@holbertonschool.com>
2019-11-05 13:38:04 +0000

    Free integer list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free integer list.
+ * @head: Head of the list.
+ *
+ * Return: Nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = NULL;
+
+	if (!*head)
+	{
+		return;
+	}
+	while (*head)
+	{
+		tmp = (**head).next;
+		free(*head);
+		*head = tmp;
+	}
+}
