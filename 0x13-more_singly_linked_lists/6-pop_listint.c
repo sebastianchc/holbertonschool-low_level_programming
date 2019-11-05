681c579f01697b378d532905d5fa64481f552f51
sebastianchc <1200@holbertonschool.com>
2019-11-05 14:43:01 +0000

    Delete head list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Delete the head list.
+ * @head: Head list.
+ *
+ * Return: Head node’s data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	if (!head)
+	{
+		return(0);
+	}
+	tmp = *head;
+	n = (**head).n;
+	*head = (*tmp).next;
+	free(tmp);
+	return(n);
+}
