b74cd52445d81a69913f10f994a3f2346cbe19c2
sebastianchc <1200@holbertonschool.com>
2019-11-06 14:27:52 +0000

    Insert new node wherever

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,8 +14,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *tmp;
 	unsigned int i;
 
+	if (!head)
+	{
+		return (0);
+	}
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node || !head)
+	if (!new_node)
 	{
 		return (0);
 	}
