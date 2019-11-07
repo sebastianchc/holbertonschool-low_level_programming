52e3ec5e0533557b54f94dd9eade04066c640639
sebastianchc <1200@holbertonschool.com>
2019-11-07 17:43:30 +0000

    [Fix]Get bit value

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,19 +12,15 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	i = 0;
-	if (n == 0)
+	if (index > 63)
 	{
-		return (n & 1);
+		return (-1);
 	}
+	i = 0;
 	while (i < index)
 	{
 		n = n >> 1;
 		i++;
-	}
-	if (!(n | 0))
-	{
-		return (-1);
 	}
 	return (n & 1);
 }
