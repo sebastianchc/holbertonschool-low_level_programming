d53a3107b9b210f05a3bd06b8f8abb052324ddc8
sebastianchc <1200@holbertonschool.com>
2019-11-07 17:55:29 +0000

    Set the value of a bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Set the value of a bot to 1.
+ * @n: Number.
+ * @index: Position.
+ *
+ * Return: New number.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+	unsigned long int count;
+
+	count = 1;
+	if (index > 63)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (i < index)
+	{
+		count *= 2;
+		i++;
+	}
+	*n = *n | count;
+	return (*n);
+}
