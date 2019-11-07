efb7189334c52209b9a1d9c76619e9ee31063f56
sebastianchc <1200@holbertonschool.com>
2019-11-07 18:29:04 +0000

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Set the value of a bot to 1.
+ * @n: Number.
+ * @index: Position.
+ *
+ * Return: New number.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+	unsigned long int count;
+
+	count = 1;
+	if (index > 63)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (i < index)
+	{
+		count *= 2;
+		i++;
+	}
+	if (*n ^ count)
+	{
+		return (1);
+	}
+	*n ^= count;
+	return (1);
+}
