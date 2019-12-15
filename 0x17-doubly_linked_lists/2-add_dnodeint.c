610ba2f9a6c2515fca166e4c45af7fce0702a2c2
sebastianchc <1200@holbertonschool.com>
2019-12-15 18:49:00 +0000

    [Update] Considering prev pointer when head exists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,10 +12,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (!head)
-	{
-		return (0);
-	}
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 	{
@@ -24,6 +20,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	(*new).n = n;
 	(*new).prev = NULL;
 	(*new).next = *head;
+	if (*head)
+	{
+		(**head).prev = new;
+	}
 	*head = new;
 	return (new);
 }
