1af9ef7efe6440ea6596dea5af92bf9040fc5aae
sebastianchc <1200@holbertonschool.com>
2019-12-15 18:50:50 +0000

    [Update] Releasing a doubly link list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list
+ * @head: Pointer to list to be freed
+ *
+ * Return: Nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = (*head).next;
+		free(current);
+        }
+}
