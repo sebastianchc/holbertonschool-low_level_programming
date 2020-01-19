ef1e5b0ea2877f5287a9dd87ec517f5359c45da5
sebastianchc <1200@holbertonschool.com>
2020-01-19 21:37:27 +0000

    [Fix] Betty style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->key = strdup(key);
 	new->value = strdup(value);
 	ht->array[index] = new;
-	if(!ht->array[index])
+	if (!ht->array[index])
 	{
 		new->next = NULL;
 		return (1);
