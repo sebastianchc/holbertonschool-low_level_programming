f7b01a0e7d855dacf4ba07f4b118f09b83b1c1af
sebastianchc <1200@holbertonschool.com>
2020-02-21 22:09:47 -0500

    [Feature] Using two if statement to considering NULL

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,14 +3,26 @@ def island_perimeter(grid):
     """ islan_perimeter function """
     count = 0
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                if i + 1 < len(grid):
+                    if grid[i + 1][j] == 0:
+                        count += 1
+                else:
                     count += 1
-                if i == 0 or grid[i - 1][j] == 0:
+                if i - 1 >= 0:
+                    if grid[i - 1][j] == 0:
+                        count += 1
+                else:
                     count += 1
-                if j == len(grid) - 1 or grid[i][j + 1] == 0:
+                if j + 1 < len(grid[0]):
+                    if grid[i][j + 1] == 0:
+                        count += 1
+                else:
                     count += 1
-                if j == 0 or grid[i][j - 1] == 0:
+                if j - 1 >= 0:
+                    if grid[i][j - 1] == 0:
+                        count += 1
+                else:
                     count += 1
     return count
